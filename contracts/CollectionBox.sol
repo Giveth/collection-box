034ac7f2fe5dafff284c180da624f4b0ce263dd3
BhagyaRaj Killamsetty <sameerkillamsetty@gmail.com>
2018-10-10 00:28:24 +0530

    Update CollectionBox.sol

--- contracts/CollectionBox.sol
+++ contracts/CollectionBox.sol
@@ -1,48 +1,128 @@
 pragma solidity ^0.4.21;
 
-import "giveth-bridge/contracts/GivethBridge.sol";
+import "giveth-common-contracts/contracts/Escapable.sol";
+import './lib/BridgeInterface.sol';
+import './lib/Pausable.sol';
 
-contract CollectionBox  is GivethBridge{
-    
-    address DACAddress = 0xC59dCE5CCC065A4b51A2321F857466A25ca49B40;
-    
-    struct donatorsInfo {
+contract CollectionBox is BridgeInterface, Escapable, Pausable {
+
+    address bridgeAddress = 0xC59dCE5CCC065A4b51A2321F857466A25ca49B40;
+    mapping(address => bool) tokenWhitelist;
+    uint64 receiverId;
+    uint transactionsCount = 0;
+    event DonateAndCreateGiver(address giver, uint64 receiverId, address token, uint amount);
+
+
+    struct transaction {
+        address donator;
+        address token;
         uint donation;
-        bool exists;
     }
-    
-    mapping (address => donatorsInfo) donators;
-    
-    function CollectionBox (
+
+    transaction[] public transactions;
+ 
+    constructor (
         address _escapeHatchCaller,
         address _escapeHatchDestination,
-        uint _absoluteMinTimeLock,
-        uint _timeLock,
-        address _securityGuard,
-        uint _maxSecurityGuardDelay
-    ) GivethBridge(
+        uint64 _receiverId
+    ) Escapable (
         _escapeHatchCaller,
-        _escapeHatchDestination,
-        _absoluteMinTimeLock,
-        _timeLock,
-        _securityGuard,
-        _maxSecurityGuardDelay
-    )public 
+        _escapeHatchDestination
+    ) public
     {
-    }
-    
-    function donate()  payable external {
-        
-        if(donators[msg.sender].exists)
-            donators[msg.sender].donation = donators[msg.sender].donation + msg.value;
-        else
-             donators[msg.sender].donation =  msg.value;
-        
-        DACAddress.transfer(msg.value);
-    }
-    
-    function getDonatorInfo(address donator) public view returns(uint) {
-        return donators[donator].donation;
+        receiverId = _receiverId;
+        tokenWhitelist[0] = true; // enable eth transfers
     }
 
+    function getBalance() view public returns (uint){
+        return this.balance;
+    }
+
+    function() payable external {
+
+        require(msg.value > 0);
+        
+        transaction memory newTransaction = transaction({
+            donator : msg.sender,
+            donation : msg.value,
+            token : 0x000
+        });
+
+        if(transactionsCount == transactions.length) {
+            transactions.length += 1;
+        }
+
+        transactions[transactionsCount++] = newTransaction;
+    }
+
+    function donateERC20Tokens(address _token, uint _amount) whenNotPaused payable public {
+        
+        uint amount = _receiveDonation(_token, _amount);
+         
+        transaction memory newTransaction = transaction({
+            donator : msg.sender,
+            donation : amount,
+            token : _token
+        });
+        
+        if(transactionsCount == transactions.length) {
+            transactions.length += 1;
+        }
+
+        transactions[transactionsCount++] = newTransaction;
+    }
+    
+    /**
+    * The `owner` can call this function to add/remove a token from the whitelist
+    *
+    * @param token The address of the token to update
+    * @param accepted Wether or not to accept this token for donations
+    */
+    function whitelistToken(address token, bool accepted) whenNotPaused onlyOwner external {
+        tokenWhitelist[token] = accepted;
+    }
+
+    function transferToBridge() external onlyOwner payable {
+
+        for(uint i = 0; i < transactionsCount; i++) {
+            donateAndCreateGiver(transactions[i].donator, receiverId, transactions[i].token, transactions[i].donation);
+        
+            if(transactions[i].token != 0x000) {
+                ERC20(transactions[i].token).transferFrom(this, bridgeAddress, transactions[i].donation);
+            } 
+        }
+        bridgeAddress.transfer(this.balance);
+        clearTransactions();
+
+    }
+
+    function clearTransactions() internal {
+        transactionsCount = 0;
+    }
+
+    function donateAndCreateGiver(address giver, uint64 _receiverId, address token, uint _amount) whenNotPaused private {
+        require(giver != 0);
+        require(_receiverId != 0);
+        emit DonateAndCreateGiver(giver, _receiverId, token, _amount);
+    }
+
+   
+    /**
+    * @dev used to actually receive the donation. Will transfer the token to to this contract
+    */
+    function _receiveDonation(address token, uint _amount) internal returns(uint amount) {
+        require(tokenWhitelist[token]);
+        amount = _amount;
+
+        // eth donation
+        if (token == 0x000) {
+            amount = msg.value;
+        }
+
+        require(amount > 0);
+
+        if (token != 0) {
+           require(ERC20(token).transferFrom(msg.sender, this, amount));
+        }
+    }
 }
